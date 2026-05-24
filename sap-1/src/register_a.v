13a192bb510f687b072dcefe57e049e0b33b3554
Batu Gürelli <ba2gtx@gmail.com>
2026-05-24 11:01:14 +0200

    add register-a module

---
+++ sap-1/src/register_a.v
@@ -0,0 +1,23 @@
+module register_a (
+    input wire i_clk, //clock signal
+    input wire i_rst, //reset flag
+    input wire i_load, //load flag
+    input wire [7:0] i_bus, //bus input
+    output wire [7:0] o_data 
+);
+    reg [7:0] register_a;
+
+    always @(posedge i_clk) begin
+
+        if (i_rst) begin
+            register_a <= 8'b0;
+        end
+        else if (i_load) begin
+            register_a <= i_bus;
+        end
+        
+    end
+
+    assign o_data = register_a;
+    
+endmodule
