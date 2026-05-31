1ab55561083312efa97a2ad84c7f80490c2b8094
Batu Gürelli <ba2gtx@gmail.com>
2026-05-31 22:00:52 +0200

    add the stages for each operand and fix the signal names

--- sap-1/src/controller.v
+++ sap-1/src/controller.v
@@ -2,11 +2,11 @@ module controller (
     input wire i_clk,
     input wire i_rst,
     input wire [3:0] i_opcode
-    output wire [11:0] o_status
+    output wire [11:0] o_control
 );
     //define control signals
     localparam SIG_ADDER_EN = 0;
-    localparam SIG_ADDER_EN = 1;
+    localparam SIG_ADDER_SUB = 1;
     localparam SIG_B_LOAD = 2;
     localparam SIG_A_EN = 3;
     localparam SIG_A_LOAD = 4;
@@ -23,5 +23,103 @@ module controller (
     localparam OP_ADD = 4'b0001;
     localparam OP_SUB = 4'b0010;
     localparam OP_HLT = 4'b1111;
+    
+    //define states
+    localparam STAGE_0 = 0;
+    localparam STAGE_1 = 1;
+    localparam STAGE_2 = 2;
+    localparam STAGE_3 = 3;
+    localparam STAGE_4 = 4;
+    localparam STAGE_5 = 5;
+
+    reg [3:0] state_reg; //register to store current state
+    reg [11:0] control_reg; //control register
+
+    //stage logic
+    always @(*) begin
+        control_reg = 12'b0; //init control register with default values
+
+        case (state_reg)
+            /****************************************
+            common stage operations
+            ****************************************/
+            
+            STAGE_0: begin
+                control_reg[SIG_PC_EN] = 1; //enable bus mode
+                control_reg[SIG_MEM_LOAD] = 1; //load the value to memory
+            end
+
+            STAGE_1: begin
+                control_reg[SIG_PC_INC] = 1; //increment PC
+            end
+
+            STAGE_2: begin
+                control_reg[SIG_MEM_EN] = 1; //load the data on memory to bus
+                control_reg[SIG_IR_LOAD] = 1; //load the IR
+            end
+
+            /****************************************
+            instruction specific stage operations
+            ****************************************/
+            STAGE_3: begin
+                case(i_opcode)
+                    OP_LDA: begin
+                        control_reg[SIG_IR_EN] = 1; //put operand to bus
+                        control_reg[SIG_MEM_LOAD] = 1; //load the operand to memory
+                    end
+
+                    OP_ADD: begin
+                        control_reg[SIG_IR_EN] = 1; //put operand to bus
+                        control_reg[SIG_MEM_LOAD] = 1; //load the operand to memory
+                    end
+
+                    OP_SUB: begin
+                        control_reg[SIG_IR_EN] = 1; //put operand to bus
+                        control_reg[SIG_MEM_LOAD] = 1; //load the operand to memory
+                    end
+
+                    OP_HLT: begin
+                        control_reg[SIG_HLT] = 1; //halt the clock
+                    end
+                endcase
+            end
+
+            STAGE_4: begin
+                case(i_opcode)
+                    OP_LDA: begin
+                        control_reg[SIG_MEM_EN] = 1; //put data in memory to bus
+                        control_reg[SIG_A_LOAD] = 1; //load the data on bus to A
+                    end
+
+                    OP_ADD: begin
+                        control_reg[SIG_MEM_EN] = 1; //put data in memory to bus
+                        control_reg[SIG_B_LOAD] = 1; //load the data on bus to B
+                    end
+
+                    OP_SUB: begin
+                        control_reg[SIG_MEM_EN] = 1; //put data in memory to bus
+                        control_reg[SIG_B_LOAD] = 1; //load the data on bus to B
+                    end
+                endcase
+            end
+
+            STAGE_5: begin
+                case(i_opcode)
+                OP_ADD begin:
+                    control_reg[SIG_ADDER_EN] = 1; //put the data on adder to bus
+                    control_reg[SIG_A_LOAD] = 1; //load the data to A
+                end
+
+                OP_SUB begin:
+                    control_reg[SIG_ADDER_SUB] = 1; //enable subtraction
+                    control_reg[SIG_ADDER_EN] = 1; //put the data on adder to bus
+                    control_reg[SIG_A_LOAD] = 1; //load the data to A
+                end
+                endcase
+            end
+        endcase
+    end
+
+    assign o_control = control_reg;
 
 endmodule
