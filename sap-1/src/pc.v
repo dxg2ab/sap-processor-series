6c2b47df7d649683439d8976452b912fc5054c24
Batu Gürelli <ba2gtx@gmail.com>
2026-05-31 21:48:28 +0200

    improve comments

--- sap-1/src/pc.v
+++ sap-1/src/pc.v
@@ -1,18 +1,20 @@
 module pc (
-    input i_clk,
-    input i_rst, //reset flag
-    input i_inc, //increase flag
-    output [7:0] o_adr
+    input wire i_clk, //clock signal
+    input wire i_rst, //reset flag
+    input wire i_inc, //increase flag
+    output [7:0] o_adr //current adress
 );
-    reg [3:0] pc //16 adresses
+    reg [3:0] pc //4 bit register to store instructions
 
     always @(posedge i_clk) begin
+
         if (i_rst) begin
             pc <= 4'b0;
         end
         else if (i_inc) begin
             pc <= pc +1;
         end
+
     end
 
     assign o_adr = pc;
