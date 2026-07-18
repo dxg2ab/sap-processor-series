eed99782a9ca14608e91ffef0fa4e651ac21fd45
Batu Gürelli <ba2gtx@gmail.com>
2026-07-18 11:21:07 +0200

    convert adder module to .sv and add comments

---
+++ sap-1/src/adder.sv
@@ -0,0 +1,11 @@
+module adder (
+    input logic i_sub, //flag to choose between addition and subtraction
+    input logic [7:0] i_a, //data from a register
+    input logic [7:0] i_b, //data from b register
+    output logic [7:0] o_result //output of the desired operation
+);
+
+    //continiously caclulate the desired operation
+    assign o_result = i_sub ? i_a - i_b : i_a + i_b;
+    
+endmodule
