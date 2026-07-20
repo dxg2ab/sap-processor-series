efdbfe6ec223a2056b11e9c66e2d950a92b1f8a5
Batu Gürelli <ba2gtx@gmail.com>
2026-07-20 07:45:59 +0200

    convert register b to system verilog

---
+++ sap-1/src/register_b.sv
@@ -0,0 +1,23 @@
+module register_b(
+    input logic i_clk, // clock signal
+    input logic i_rst, // reset signal
+    input logic i_load, // load signal
+    input logic [7:0] i_bus // data from bus
+    output logic [7:0] o_data // data out
+);
+    logic [7:0] register_b;
+
+    always_ff @(posedge i_clk) begin
+        
+        if (i_rst) begin
+            register_b <= 8'b0;
+        end
+        else if (i_load) begin
+            register_b <= i_bus;
+        end
+
+    end
+
+    assign o_data <= register_b;
+
+endmodule
