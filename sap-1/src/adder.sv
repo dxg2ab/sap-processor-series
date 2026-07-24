d99d52ab37567fb5be7ab3fb832543a2f7535994
Batu Gürelli <ba2gtx@gmail.com>
2026-07-24 09:48:44 +0200

    improve comments

--- sap-1/src/adder.sv
+++ sap-1/src/adder.sv
@@ -1,8 +1,8 @@
 module adder (
-    input logic i_sub, //flag to choose between addition and subtraction
-    input logic [7:0] i_a, //data from a register
-    input logic [7:0] i_b, //data from b register
-    output logic [7:0] o_result //output of the desired operation
+    input logic i_sub, // flag to choose between addition and subtraction
+    input logic [7:0] i_a, // data from a register
+    input logic [7:0] i_b, // data from b register
+    output logic [7:0] o_result // output of the desired operation
 );
 
     //continiously caclulate the desired operation
